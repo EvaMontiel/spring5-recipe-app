a93803944f980add2103e71b0a2c8318151303c7
Eva <evamontielaraque@gmail.com>
2022-06-08 11:37:04 +0200

    JPA Enumerations

--- src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -25,10 +27,12 @@ public class Recipe {
 	private String source;
 	private String url;
 	private String directions;
-	// private Difficulty difficulty;
 
 	@Lob
 	private Byte[] image;
+
+	@Enumerated(value = EnumType.STRING)
+	private Difficulty difficulty;
 
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Notes notes;
@@ -106,6 +110,14 @@ public class Recipe {
 
 	public void setImage(Byte[] image) {
 		this.image = image;
+	}
+
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
 	}
 
 	public Notes getNotes() {
